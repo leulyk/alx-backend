33acfc211d24bce247644bb126e4017d7c0ab962
Leul Yiheyis <leulykpro@gmail.com>
2022-05-10 20:17:14 +0300

    add the default locale and timezone

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -11,6 +11,8 @@ from flask_babel import Babel
 class Config:
     """ class that acts as a template for language configuration """
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app = Flask(__name__)

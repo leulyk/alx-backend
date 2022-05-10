28e837f37da8681ce2f1492d88848de980c20761
Leul Yiheyis <leulykpro@gmail.com>
2022-05-10 21:00:06 +0300

    flask app to test our parametrized templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""
+    i18n project: task 3
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """ class that acts as a template for language configuration """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """ route to render the index page """
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """ determines the best match with our supported languages """
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+if __name__ == '__main__':
+    app.run()

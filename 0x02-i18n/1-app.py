4f5ad8da694d7d86219a0405ab8b9b482e76f285
Leul Yiheyis <leulykpro@gmail.com>
2022-05-10 20:10:41 +0300

    setup babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""
+    Babel test 0
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """ route to render the index page """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
